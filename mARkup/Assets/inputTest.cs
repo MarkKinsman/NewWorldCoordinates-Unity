fb9534848b814782ed4c4ca243bdf4d491ddbca1
Logan Smith <lmaxsmith@gmail.com>
2019-04-27 22:49:35 -0700

    Added UI elements

--- mARkup/Assets/inputTest.cs
+++ mARkup/Assets/inputTest.cs
@@ -1,19 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.WSA.Input;
 
 public class inputTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static inputTest Instance { get; private set; }
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    // Use this for initialization
+    void Awake()
     {
-        
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.Tapped += (args) =>
+        {
+            // Send an OnSelect message to the focused object and its ancestors.
+            if (FocusedObject != null)
+            {
+                FocusedObject.SendMessageUpwards("OnSelect", SendMessageOptions.DontRequireReceiver);
+            }
+            Debug.Log("OMG I tapped");
+        };
+        recognizer.StartCapturingGestures();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Figure out which hologram is focused this frame.
+        GameObject oldFocusObject = FocusedObject;
+
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            // If the raycast did not hit a hologram, clear the focused object.
+            FocusedObject = null;
+        }
+
+        // If the focused object changed this frame,
+        // start detecting fresh gestures again.
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
     }
 }
